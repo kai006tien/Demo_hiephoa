ebe33c6ee2657e49a1cd26f1be267302280f0e76
kai006tien <truonghoangtien90@gmail.com>
2026-06-11 21:41:15 +0700

    config: Tích hợp thành công URL Google Sheets Web App và kích hoạt sync

--- js/config.js
+++ js/config.js
@@ -5,10 +5,10 @@
 const CONFIG = {
   // Đường dẫn Web App của Google Apps Script sau khi deploy
   // Ví dụ: "https://script.google.com/macros/s/AKfycbw.../exec"
-  googleScriptUrl: '',
+  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbzmTc8XsQTXsZHozVmtqv62Q3ObG5LcKaZhBT6cZuYetkGlfeqcHvXV3jrcGAXz4J3j/exec',
 
   // Trạng thái kích hoạt đồng bộ hóa qua Google Sheets
-  syncEnabled: false,
+  syncEnabled: true,
 
   // Token bí mật bảo mật truy cập Google Sheets
   secretToken: 'HiepHoaSecret2026'
