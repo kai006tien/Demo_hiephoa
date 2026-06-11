cca495567a38b01e6210a132130e64138f2d10fe
kai006tien <truonghoangtien90@gmail.com>
2026-06-11 21:53:15 +0700

    config: Cập nhật URL Web App mới AKfycbxaJlWtPX_QKi97RW7tDhGcgzOoBBw1UwawhFoNCVU6He30ZAb4yEL9qsTsO3wclMYL

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // Đường dẫn Web App của Google Apps Script sau khi deploy
   // Ví dụ: "https://script.google.com/macros/s/AKfycbw.../exec"
-  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbzmTc8XsQTXsZHozVmtqv62Q3ObG5LcKaZhBT6cZuYetkGlfeqcHvXV3jrcGAXz4J3j/exec',
+  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbxaJlWtPX_QKi97RW7tDhGcgzOoBBw1UwawhFoNCVU6He30ZAb4yEL9qsTsO3wclMYL/exec',
 
   // Trạng thái kích hoạt đồng bộ hóa qua Google Sheets
   syncEnabled: true,
