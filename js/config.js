1721635b1515350728d08f6ca174fb69718324d0
kai006tien <truonghoangtien90@gmail.com>
2026-06-11 22:45:37 +0700

    config: Sua lai chu hoa I trong URL Web App

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // Đường dẫn Web App của Google Apps Script sau khi deploy
   // Ví dụ: "https://script.google.com/macros/s/AKfycbw.../exec"
-  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbz6bn0293mGlmSWJ0is5uDsONjr3QE-MUnzDoLWo5DtES85XaR7qt8EdC7aqednCoDE/exec',
+  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbz6bn0293mGImSWJ0is5uDsONjr3QE-MUnzDoLWo5DtES85XaR7qt8EdC7aqednCoDE/exec',
 
   // Trạng thái kích hoạt đồng bộ hóa qua Google Sheets
   syncEnabled: true,
